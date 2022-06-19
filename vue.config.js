47615e90bbd1d91e84e6198091380d0bd5a11281
zhouboyi <1144188685@qq.com>
2022-06-19 13:40:44 +0800

    feat(request): 新增登录请求

--- vue.config.js
+++ vue.config.js
@@ -10,5 +10,25 @@ module.exports = {
         config.plugins.push(Components({
             resolvers: [ElementPlusResolver()],
         }))
+    },
+    devServer: {
+        host: "127.0.0.1",
+        port: 18011,
+        open: true,
+        proxy: {
+            // Vue 请求路径中包含 /api 时, 自动代理请求到服务端地址
+            '/api': {
+                ws: false,
+                https: false,
+                // 服务端 IP 地址
+                target: "http://127.0.0.1:18071",
+                // 将前端 IP 地址转换为服务端 IP 地址
+                changeOrigin: true,
+                // 删除用于匹配规则的 /api 路径
+                pathRewrite: {
+                    ["^/api"]: ""
+                }
+            }
+        }
     }
 }
