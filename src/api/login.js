fbecaceb3c2e256ee2a2875a226c2e39a11c3955
zhouboyi <1144188685@qq.com>
2022-06-28 21:37:26 +0800

    登录成功，保存token，跳转首页

--- src/api/login.js
+++ src/api/login.js
@@ -1,6 +1,12 @@
 import request from "@/api/request"
 
-export const login = (data) => {
+export const login = (params) => {
+
+    // 添加登录验证需要的请求参数
+    params.append('grant_type', 'password')
+    params.append('client_id', 'manage')
+    params.append('client_secret', '123456')
+
     return request({
         url: '/security/oauth/token',
         method: 'post',
@@ -8,6 +14,6 @@ export const login = (data) => {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Accept': '*/*'
         },
-        data
+        data: params
     })
 }
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'element-plus/dist/index.css'
 import '@/assets/scss/index.scss'
 
 createApp(App).use(store).use(router).mount('#app')
