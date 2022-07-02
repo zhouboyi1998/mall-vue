fac21f172daff5bcee353fe8f6a5c8f0ece29ca6
zhouboyi <1144188685@qq.com>
2022-07-02 22:44:21 +0800

    improve(sidebar): 调整左侧导航栏

--- src/views/home/menu/Menu.vue
+++ src/views/home/menu/Menu.vue
@@ -15,34 +15,24 @@
                 </el-icon>
                 <span>商品管理</span>
             </template>
-            <el-sub-menu index="1-1">
-                <template #title>
-                    <el-icon class="svg-container" :size="20">
-                        <Iphone/>
-                    </el-icon>
-                    <span>分类</span>
-                </template>
-                <el-menu-item index="1-1-1">分类列表</el-menu-item>
-            </el-sub-menu>
-            <el-sub-menu index="1-2">
-                <template #title>
-                    <el-icon class="svg-container" :size="20">
-                        <Apple/>
-                    </el-icon>
-                    <span>品牌</span>
-                </template>
-                <el-menu-item index="1-2-1">品牌列表</el-menu-item>
-            </el-sub-menu>
-            <el-sub-menu index="1-3">
-                <template #title>
-                    <el-icon class="svg-container" :size="20">
-                        <GoodsFilled/>
-                    </el-icon>
-                    <span>商品</span>
-                </template>
-                <el-menu-item index="1-3-1">SPU</el-menu-item>
-                <el-menu-item index="1-3-2">SKU</el-menu-item>
-            </el-sub-menu>
+            <el-menu-item index="1-1">
+                <el-icon class="svg-container" :size="20">
+                    <Iphone/>
+                </el-icon>
+                <span>分类列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-2">
+                <el-icon class="svg-container" :size="20">
+                    <Apple/>
+                </el-icon>
+                <span>品牌列表</span>
+            </el-menu-item>
+            <el-menu-item index="1-3">
+                <el-icon class="svg-container" :size="20">
+                    <GoodsFilled/>
+                </el-icon>
+                <span>商品列表</span>
+            </el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="2">
             <template #title>
@@ -51,15 +41,12 @@
                 </el-icon>
                 <span>订单管理</span>
             </template>
-            <el-sub-menu index="2-1">
-                <template #title>
-                    <el-icon class="svg-container" :size="20">
-                        <Ticket/>
-                    </el-icon>
-                    <span>订单</span>
-                </template>
-                <el-menu-item index="2-1-1">订单列表</el-menu-item>
-            </el-sub-menu>
+            <el-menu-item index="2-1">
+                <el-icon class="svg-container" :size="20">
+                    <Ticket/>
+                </el-icon>
+                <span>订单列表</span>
+            </el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="3">
             <template #title>
@@ -68,15 +55,12 @@
                 </el-icon>
                 <span>用户管理</span>
             </template>
-            <el-sub-menu index="3-1">
-                <template #title>
-                    <el-icon class="svg-container" :size="20">
-                        <UserFilled/>
-                    </el-icon>
-                    <span>用户</span>
-                </template>
-                <el-menu-item index="3-1-1">用户列表</el-menu-item>
-            </el-sub-menu>
+            <el-menu-item index="3-1">
+                <el-icon class="svg-container" :size="20">
+                    <UserFilled/>
+                </el-icon>
+                <span>用户列表</span>
+            </el-menu-item>
         </el-sub-menu>
         <el-sub-menu index="4">
             <template #title>
