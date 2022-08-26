3a429da1506288902be16f736d18f5ebcbd01f82
zhouboyi <1144188685@qq.com>
2022-08-26 17:08:39 +0800

    feat(logout): 添加退出功能

--- src/api/request.js
+++ src/api/request.js
@@ -5,9 +5,18 @@ const instance = axios.create({
     timeout: 10000
 })
 
-instance.interceptors.request.use(config => {
-    config.headers.Authorization = localStorage.getItem('token')
-    return config
+// axios 请求拦截器
+instance.interceptors.request.use(request => {
+    // 添加 Access Token 到请求头中
+    request.headers.Authorization = localStorage.getItem('token')
+    return request
+}, error => {
+    return Promise.reject(new Error(error))
+})
+
+// axios 响应拦截器
+instance.interceptors.response.use(response => {
+    return response
 }, error => {
     return Promise.reject(new Error(error))
 })
---
+++ src/views/index/header/components/Avatar.vue
@@ -0,0 +1,48 @@
+<template>
+    <el-dropdown class="dropdown">
+    <span class="el-dropdown-link">
+        <el-avatar shape="circle">
+            <span class="avatar-text">🚀</span>
+        </el-avatar>
+    </span>
+        <template #dropdown>
+            <el-dropdown-menu>
+                <el-dropdown-item @click="handleLogout">退出系统</el-dropdown-item>
+            </el-dropdown-menu>
+        </template>
+    </el-dropdown>
+</template>
+
+<script setup>
+import router from '@/router'
+import { ElMessage } from 'element-plus'
+
+// 退出处理
+const handleLogout = () => {
+    // 清空 Local Storage
+    localStorage.clear()
+    // 跳转到登录页
+    router.replace('/login')
+    // 退出成功提示
+    ElMessage({ message: '退出成功', type: 'success' })
+}
+</script>
+
+<style lang="scss" scoped>
+/* 头像文本 */
+.avatar-text {
+    font-size: 28px;
+}
+
+/* 下拉框 */
+.dropdown {
+    /* 设置鼠标悬停样式 */
+    cursor: pointer;
+}
+
+/* 下拉框项 */
+:deep(.el-dropdown-menu__item) {
+    /* 设置不换行 */
+    white-space: nowrap;
+}
+</style>
