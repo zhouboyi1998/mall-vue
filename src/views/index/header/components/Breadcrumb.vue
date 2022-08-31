0f9ece96b453d3f49fb664e267573e3f1f5c1d8c
zhouboyi <1144188685@qq.com>
2022-08-31 11:48:55 +0800

    feat(header): 面包屑加上横向滚动条

--- src/views/index/header/components/Breadcrumb.vue
+++ src/views/index/header/components/Breadcrumb.vue
@@ -1,16 +1,18 @@
 <template>
-    <el-breadcrumb class="breadcrumb-container" separator="/">
-        <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index">
-            <!-- 面包屑中当前页面置灰, 不能点击跳转 -->
-            <span class="breadcrumb-no-redirect" v-if="item.name === currentBreadcrumb.name">
-                {{ item.name }}
-            </span>
-            <!-- 其它面包屑可以点击跳转 -->
-            <span class="breadcrumb-redirect" v-else @click="handleRedirect(item.path)">
-                {{ item.name }}
-            </span>
-        </el-breadcrumb-item>
-    </el-breadcrumb>
+    <el-scrollbar>
+        <el-breadcrumb class="breadcrumb-container" separator="/">
+            <el-breadcrumb-item v-for="(item, index) in breadcrumbList" :key="index">
+                <!-- 当前页面的面包屑不可以点击跳转 -->
+                <span class="breadcrumb-no-redirect" v-if="item.name === currentBreadcrumb.name">
+                    {{ item.name }}
+                </span>
+                <!-- 其它页面的面包屑可以点击跳转 -->
+                <span class="breadcrumb-redirect" v-else @click="handleRedirect(item.path)">
+                    {{ item.name }}
+                </span>
+            </el-breadcrumb-item>
+        </el-breadcrumb>
+    </el-scrollbar>
 </template>
 
 <script setup>
