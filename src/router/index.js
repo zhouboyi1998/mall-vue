dafff304349970f4ae6020541cba0f1a4e412389
zhouboyi <1144188685@qq.com>
2022-11-18 17:48:01 +0800

    revert(router): 取消路由路径修改

--- src/router/index.js
+++ src/router/index.js
@@ -13,42 +13,42 @@ const routes = [
                 component: () => import('../views/index/main/home/Home')
             },
             {
-                path: '/category',
+                path: '/goods/category',
                 name: 'Category',
                 component: () => import('../views/index/main/category/Category')
             },
             {
-                path: '/brand',
+                path: '/goods/brand',
                 name: 'Brand',
                 component: () => import('../views/index/main/brand/Brand')
             },
             {
-                path: '/goods',
+                path: '/goods/goods',
                 name: 'Goods',
                 component: () => import('../views/index/main/goods/Goods')
             },
             {
-                path: '/order',
+                path: '/order/order',
                 name: 'Order',
                 component: () => import('../views/index/main/order/Order')
             },
             {
-                path: '/member',
+                path: '/member/member',
                 name: 'Member',
                 component: () => import('../views/index/main/member/Member')
             },
             {
-                path: '/user',
+                path: '/user/user',
                 name: 'User',
                 component: () => import('../views/index/main/user/User')
             },
             {
-                path: '/role',
+                path: '/user/role',
                 name: 'Role',
                 component: () => import('../views/index/main/role/Role')
             },
             {
-                path: '/menu',
+                path: '/user/menu',
                 name: 'Menu',
                 component: () => import('../views/index/main/menu/Menu')
             }
