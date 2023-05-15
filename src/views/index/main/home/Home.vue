750d7465c0cfefc9b90aaba154b1bd1da731006a
zhouboyi <1144188685@qq.com>
2023-05-15 15:52:16 +0800

    feat(home): 添加首页UI

--- src/views/index/main/home/Home.vue
+++ src/views/index/main/home/Home.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>home</div>
+    <div class="home-container">
+        <div class="title-container">
+            <span class="title-left">&nbsp;&nbsp;mall</span>
+            <span class="title-middle">-</span>
+            <span class="title-right">cloud</span>
+        </div>
+    </div>
 </template>
 
 <script setup></script>
