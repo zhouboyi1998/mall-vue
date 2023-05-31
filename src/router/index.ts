ba084c1ade34421d3cb746703ca66907e2fa0557
zhouboyi <1144188685@qq.com>
2023-05-31 17:33:33 +0800

    refactor(router): Vue-Router导入页面组件时使用@路径别名

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,80 +4,80 @@ const routes = [
     {
         path: '/',
         name: 'Index',
-        component: () => import('../views/index/Index.vue'),
+        component: () => import('@/views/index/Index.vue'),
         redirect: '/home',
         children: [
             {
                 path: '/home',
                 name: 'Home',
-                component: () => import('../views/index/main/home/Home.vue')
+                component: () => import('@/views/index/main/home/Home.vue')
             },
             {
                 path: '/mall-goods/category',
                 name: 'Category',
-                component: () => import('../views/index/main/category/Category.vue')
+                component: () => import('@/views/index/main/category/Category.vue')
             },
             {
                 path: '/mall-goods/brand',
                 name: 'Brand',
-                component: () => import('../views/index/main/brand/Brand.vue')
+                component: () => import('@/views/index/main/brand/Brand.vue')
             },
             {
                 path: '/mall-goods/goods',
                 name: 'Goods',
-                component: () => import('../views/index/main/goods/Goods.vue')
+                component: () => import('@/views/index/main/goods/Goods.vue')
             },
             {
                 path: '/mall-order/order',
                 name: 'Order',
-                component: () => import('../views/index/main/order/Order.vue')
+                component: () => import('@/views/index/main/order/Order.vue')
             },
             {
                 path: '/mall-member/member',
                 name: 'Member',
-                component: () => import('../views/index/main/member/Member.vue')
+                component: () => import('@/views/index/main/member/Member.vue')
             },
             {
                 path: '/mall-user/user',
                 name: 'User',
-                component: () => import('../views/index/main/user/User.vue')
+                component: () => import('@/views/index/main/user/User.vue')
             },
             {
                 path: '/mall-user/role',
                 name: 'Role',
-                component: () => import('../views/index/main/role/Role.vue')
+                component: () => import('@/views/index/main/role/Role.vue')
             },
             {
                 path: '/mall-user/menu',
                 name: 'Menu',
-                component: () => import('../views/index/main/menu/Menu.vue')
+                component: () => import('@/views/index/main/menu/Menu.vue')
             },
             {
                 path: '/mall-system/area',
                 name: 'Area',
-                component: () => import('../views/index/main/area/Area.vue')
+                component: () => import('@/views/index/main/area/Area.vue')
             },
             {
                 path: '/mall-system/theme',
                 name: 'Theme',
-                component: () => import('../views/index/main/theme/Theme.vue')
+                component: () => import('@/views/index/main/theme/Theme.vue')
             }
         ]
     },
     {
         path: '/login',
         name: 'Login',
-        component: () => import('../views/login/Login.vue')
+        component: () => import('@/views/login/Login.vue')
     },
     {
         path: '/401',
         name: '401',
-        component: () => import('../views/error/401.vue')
+        component: () => import('@/views/error/401.vue')
     },
     {
         path: '/404',
         name: '404',
-        component: () => import('../views/error/404.vue')
+        component: () => import('@/views/error/404.vue')
     }
 ]
 
